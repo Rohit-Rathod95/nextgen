53b09eaebb1701a82f9ad4991f6eaeba44e56634
Rohit Rathod <rohit1212rathod@gmail.com>
2026-02-27 07:51:29 +0530

    Deployment

--- worldsim/backend/main.py
+++ worldsim/backend/main.py
@@ -87,7 +87,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
-        "https://nextgen-4-hgvb.onrender.com",  # Deployed backend
+        "https://nextgen-48ti.vercel.app",  # Deployed frontend
     ],
     allow_credentials=True,
     allow_methods=["*"],
